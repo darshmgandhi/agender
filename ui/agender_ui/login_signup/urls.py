a12b9fa854f1ccc3aaec0ae1f34817282be3b5a7
darshmgandhi <gandhidarshm@gmail.com>
2021-11-19 07:00:38 +0530

    Integrated Age Detection

--- ui/agender_ui/login_signup/urls.py
+++ ui/agender_ui/login_signup/urls.py
@@ -7,9 +7,10 @@ urlpatterns = [
     path('logout', views.logout, name='logout'),
     path('signup', views.signup, name='signup'),
     path('new_page', views.new_page, name="new_page"),
-    path('image_process', views.image_process, name="image_process"),
+    #path('image_process', views.image_process, name="image_process"),
     path('builder', views.builder, name='builder'),
     path('form', views.form, name='form'),
     path('webcam', views.webcam, name='webcam'),
+    path('save_form', views.save_form, name = 'save_form')
 
 ]
